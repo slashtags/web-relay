9e9ca1e04162b9191ef1efe80d45412c81c1849d
nazeh <ar.nazeh@gmail.com>
2023-08-16 13:20:41 +0300

    fix: move xmlhttprequest to its own file

--- types/lib/client/xmlhttprequest-browser.d.ts
+++ types/lib/client/xmlhttprequest-browser.d.ts
@@ -1,2 +1,11 @@
-export { XMLHttpRequest };
+declare const _exports: {
+    new (): XMLHttpRequest;
+    prototype: XMLHttpRequest;
+    readonly UNSENT: 0;
+    readonly OPENED: 1;
+    readonly HEADERS_RECEIVED: 2;
+    readonly LOADING: 3;
+    readonly DONE: 4;
+};
+export = _exports;
 //# sourceMappingURL=xmlhttprequest-browser.d.ts.map
